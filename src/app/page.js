396bcc0930a89a96b918b2f364a684a750392f4b
Ssenyonjo-Andrew <ande.ssenyonjo@gmail.com>
2025-03-03 22:48:44 +0300

    small change

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ const LoginPage = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:3000/api/auth/login', {
+      const response = await fetch('http://localhost:2000/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
